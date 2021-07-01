1d9dc3bee34733b146ef0da617004538956a47af
Vallem Balu <vbnreddy5@gmail.com>
2021-07-01 13:36:42 +0530

    Employee p_a

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UC1_EMP_Wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC2_DailyEmployeeWage.DailyEmployeeWage();
+           //UC3_PartTimeEmpWage.PartTime();
+           //UC4_Case_Statement.case_statement();
+           //UC5_Wage_of_Month.Wage_of_Month();
+           //UC6_No_of_workingdays_wage.No_Of_WorkingDays_Wage();
+          //UC7_ComputeEmpWage.ComputeEmpWage();
+          //UC8_multi_comp_Wage.multi_comp_EmpWage(string company, int Emp_Rate_Per_Hour, int Num_Of_Working_Days, int Max_Hrs_In_Month);
+           Console.ReadKey();
+        }
+        /* 
+        public void Main()
+        {  //Constants
+            int Is_Full_Time = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }*/
+    }
+}
+
