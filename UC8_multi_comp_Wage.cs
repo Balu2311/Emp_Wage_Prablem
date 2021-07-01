4fcb9d2f3c7a39741eef83025794ce072341ba80
Vallem Balu <vbnreddy5@gmail.com>
2021-07-01 14:52:23 +0530

    CompanyWage

---
+++ UC8_multi_comp_Wage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1_EMP_Wage
+{
+    class UC8_multi_comp_Wage
+    {
+        public const int Is_Part_Time = 1;
+        public const int Is_Full_Time = 2;
+        /*public const int Emp_Rate_Per_Hour = 20;
+        public const int Num_Of_Working_Days = 2;
+        public const int Max_Hrs_In_Month = 10;*/
+
+        public static int multi_comp_EmpWage(string company, int Emp_Rate_Per_Hour, int Num_Of_Working_Days, int Max_Hrs_In_Month)
+        {
+            //varables
+            int empHrs = 0, TotalEmpHrs = 0, TotalWorkingDays = 0;
+            //Computaion
+            while (TotalEmpHrs <= Max_Hrs_In_Month && TotalWorkingDays < Num_Of_Working_Days)
+            {
+                TotalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case Is_Part_Time:
+                        empHrs = 4;
+                        break;
+                    case Is_Full_Time:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                TotalEmpHrs += empHrs;
+                Console.WriteLine("Days#: " + TotalWorkingDays + "  empWage : " + empHrs);
+            }
+            int TotalEmpWage = TotalEmpHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Total Emp Wage : " + TotalEmpWage);
+            return TotalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            multi_comp_EmpWage("DMart", 20, 2, 10);
+            multi_comp_EmpWage("Reliance", 10, 4, 20);
+
+        }
+    }
+}
