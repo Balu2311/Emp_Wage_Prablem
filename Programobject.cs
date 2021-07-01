7bb548bdf95dec2cfa7bd54334dab3a6a7369f03
Vallem Balu <vbnreddy5@gmail.com>
2021-07-01 17:01:04 +0530

    Finel_ object model

---
+++ Programobject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
